28a6402cd1298c3c602cfbbfd2807dbe7f3fd554
albertoBlancoCastro <62385861+albertoBlancoCastro@users.noreply.github.com>
2025-05-09 14:44:35 +0100

    Update loadConfiguration.m
    More initial fixing ...

--- software/conf/loadConfiguration.m
+++ software/conf/loadConfiguration.m
@@ -19,8 +19,6 @@ time2Show = [time1 now];
 name                = SYSTEMNAME;             
 [configuration, devicePos] = initNoReadableDevice({name,configuration,devicePos,SYS,OS});
 telescopePath = [configuration.dev(1).path.data 'ana' b];
-%%%%%%%%%%%%%%%%%%%%%
-
 
 %hours
 downScaling = 1;
@@ -39,28 +37,7 @@ mkdirOS(SYS,OS,1);
 mkdirOS([SYS 'devices' b],OS,1);
 mkdirOS([SYS 'logs' b],OS,1);
 
-%%% Devices %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Device 01     CONTROLLER 01                     
-name                = 'CONTROLER01';                                       %'DeviceName01';                   %This is the name of the device
-IP                  = 'hadesfrpcgas';                                      %'DeviceIP';                       %Device IP. This relay on .ssh/config
-fileExt             = 'GAS11*.log';                                        %'filePatter*.*';                  %File Pattern to match
-remotePath          = '/home/rpcuser/logs/';                               %'/home/rpcuser/logs/';            %Path of the file to read
-dcData2MatScript    = 'procGenericLog';                                    %'procGenericLog';                 %Name of the processing script 
-type                = 'I2C';                                               %'I2C';                            %Format of the timestamp of file to read       
-columns             = 6;                                                   %6;                                %Number of columns to read
-nameFormat          = '******2024-03-01****';                              %'******2024-03-01****';           %Name of the new name. * will be removed
-distributionLT      = 'lookUpTablefRPCController.m';                       %'lookUpTablefRPCController.m';    %Lookuptable of the device
-
-[configuration, devicePos] = initReadableDevice({name,IP,fileExt,remotePath,dcData2MatScript,type,columns,nameFormat,distributionLT,configuration,devicePos,SYS,OS});
-
-%% Device 02     GASSYSTEM 
-name                = 'GASSYSTEM';                                         %'RPC01';                          %This is the name of the device
-[configuration, devicePos] = initNoReadableDevice({name,configuration,devicePos,SYS,OS});
-
-
-
-
+loadMyDevices.m
 
 
 %% TRB    
