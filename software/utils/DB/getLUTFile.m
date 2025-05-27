53b6f803a6091f58abf1dcaa18b2a1fdf7ca34c6
albertoBlancoCastro <alberto@lip.pt>
2025-05-27 16:56:24 +0100

    Modification of code to make it compatible with Octave

--- software/utils/DB/getLUTFile.m
+++ software/utils/DB/getLUTFile.m
@@ -1,8 +1,9 @@
 function LUTfile = getLUTFile(dev)
     LUTfile = fullfile(dev.dcs.path.LT, dev.dcs.distributionLT);
-    if ~endsWith(LUTfile, '.m')
+    if length(LUTfile) < 2 || ~strcmp(LUTfile(end-1:end), '.m')
         LUTfile = [LUTfile '.m'];
     end
+
     if ~exist(LUTfile, 'file')
         LUTfile = '';
     end
--- software/utils/DB/isValidDevice.m
+++ software/utils/DB/isValidDevice.m
@@ -4,10 +4,3 @@ function valid = isValidDevice(dev)
             isfield(dev.dcs, 'path') && isfield(dev.dcs, 'distributionLT') && isfield(dev.dcs.path, 'LT');
 end
 
-% getLUTFile.m
-function LUTfile = getLUTFile(dev)
-    LUTfile = fullfile(dev.dcs.path.LT, dev.dcs.distributionLT);
-    if ~endsWith(LUTfile, '.m')
-        LUTfile = [LUTfile '.m'];
-    end
-end
